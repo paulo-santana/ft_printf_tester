a937b63909b159d751a9ad2d16c50bfdae566b05
Paulo Santana <paulo.santana.r@gmail.com>
2021-07-08 16:17:00 -0300

    feat: better output of non printable chars

--- libtest/print_errors.c
+++ libtest/print_errors.c
@@ -9,11 +9,35 @@ void print_int_diff(int expected, int result)
 	tester_putstr(RESET "\n");
 }
 
+void print_non_print(char c)
+{
+	char cx[3];
+	int size = 0;
+	char *hexmap = "0123456789abcdef";
+
+	if (c >= 16)
+	{
+		cx[0] = '\\';
+		cx[1] = hexmap[c / 16];
+		cx[2] = hexmap[c % 16];
+		size = 3;
+	}
+	else
+	{
+		cx[0] = '\\';
+		cx[1] = hexmap[c % 16];
+		size = 2;
+	}
+	tester_putstr(MAGENTA);
+	write(1, cx, size);
+	tester_putstr(RESET);
+}
+
 void	print_string_diff(char expected[], char result[], unsigned int expected_size, unsigned int result_size)
 {
 	int is_red_already = 0;
 
-	tester_putstr(RESET "        Expected: [");
+	tester_putstr(RESET "        Expected: \"");
 	for (unsigned int i = 0; i < expected_size; i++)
 	{
 		if (expected[i] == '\0')
@@ -21,12 +45,12 @@ void	print_string_diff(char expected[], char result[], unsigned int expected_siz
 		else if (expected[i] == '\n')
 			tester_putstr(MAGENTA "\\n" RESET);
 		else if (!isprint(expected[i]))
-			tester_putstr(MAGENTA "*");
+			print_non_print(expected[i]);
 		else
 			tester_putchar(expected[i]);
 	}
 
-	tester_putstr("]\n        Got:      [");
+	tester_putstr("\"\n        Got:      \"");
 	unsigned int i = 0;
 	int expected_ended = 0;
 	while (i < result_size)
@@ -44,7 +68,7 @@ void	print_string_diff(char expected[], char result[], unsigned int expected_siz
 		if (result[i] == '\0')
 			tester_putstr(BOLD MAGENTA "\\0" RESET);
 		else if (!isprint(result[i]))
-			tester_putchar('*');
+			print_non_print(result[i]);
 		else
 			tester_putchar(result[i]);
 		if (result[i] == '\0')
@@ -53,5 +77,5 @@ void	print_string_diff(char expected[], char result[], unsigned int expected_siz
 			expected_ended = 1;
 		i++;
 	}
-	tester_putstr(RESET "]\n");
+	tester_putstr(RESET "\"\n");
 }
