2298ccc11286b30cd1d691b465b4b76686f68b77
Paulo Santana <paulo.santana.r@gmail.com>
2021-07-09 09:05:17 -0300

    chore: automated commit

--- libtest/diff.c
+++ libtest/diff.c
@@ -3,21 +3,15 @@
 int test_string(char *expected, char *got, int limit)
 {
 	int success = 1;
-	//int len = 0;
 
-	if (expected == NULL)
-	{
+	if (expected == NULL) {
 		if (got != NULL)
 			success = 0;
-	}
-	else if (got == NULL)
+	} else if (got == NULL)
 		success = 0;
-	else if (memcmp(expected, got, limit) != 0)
-	{
+	else if (memcmp(expected, got, limit) != 0) {
 		success = 0;
 		//len = strlen(expected) + 1;
 	}
-	//if (!success)
-	//	print_string_diff(expected, got, len);
 	return (success);
 }
