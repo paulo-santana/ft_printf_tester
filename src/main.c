2875b74ab762edf8e4d16866a6a8dfc9edfa509d
Paulo Santana <paulo.santana.r@gmail.com>
2021-07-09 16:42:27 -0300

    chore: automated commit

--- src/main.c
+++ src/main.c
@@ -1654,7 +1654,7 @@ int main(int argc, char *argv[])
 	PRINTF_EXPECTED(("the world is 2%*% more big today", 10), /* expected: */ ("the world is 2         %% more big today"));
 	PRINTF_EXPECTED(("%*%%*%%%%5%%*%", 2, 1, 9),                   /* expected: */ (" %%%%%%    %%        %%"));
 	PRINTF_EXPECTED(("%*%s%*%id%%10%*%%*%", 2, 1, 5, 9),               /* expected: */ (" %%s%%id%%10    %%        %%"));
-	PRINTF_EXPECTED(("hey, what's %5% up%%?"),             /* expected: */ ("hey, what's     %% up%%?"));
+	PRINTF_EXPECTED(("hey, what's %*% up%%?", 5),             /* expected: */ ("hey, what's     %% up%%?"));
 
 
 	tester_putstr("\n" RESET);
