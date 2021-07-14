5efc7d57475dcb1d34cc7fd5368402267d84a0b1
Paulo Santana <paulo.santana.r@gmail.com>
2021-07-14 17:11:00 -0300

    chore: automated commit

--- src/tests.c
+++ src/tests.c
@@ -1364,6 +1364,48 @@ int run_tests(int test_cat)
 	PRINTF(("%   d", 2147837));
 	PRINTF(("%  d", (int)-2147486));
 	PRINTF(("%  d this is %d getting% di hard :/", (int)-2147486, -2, 42));
+
+	right_cat = (g_all_bonus & CAT_BONUS_2) ? 1
+			: test_cat ? (test_cat & CAT_I && test_cat & CAT_BONUS_2)
+			: 1;
+	describe("\n%i and ' '");
+
+	PRINTF(("% i", 0));
+	PRINTF(("% i", 2));
+	PRINTF(("% i", -2));
+	PRINTF(("%    i", 0));
+	PRINTF(("%   i", 1));
+	PRINTF(("%  i", -1));
+	PRINTF(("% i", 2147483647));
+	PRINTF(("% i", (int)-2147483648));
+	PRINTF(("%   i", 2147483647));
+	PRINTF(("%  i", (int)-2147483648));
+	PRINTF(("% i", 2178647));
+	PRINTF(("% i", (int)-2144348));
+	PRINTF(("%   i", 2147837));
+	PRINTF(("%  i", (int)-2147486));
+	PRINTF(("%  i this is %i getting% is hari :/", (int)-2147486, -2, 42));
+
+	right_cat = (g_all_bonus & CAT_BONUS_2) ? 1
+			: test_cat ? (test_cat & CAT_D && test_cat & CAT_BONUS_2)
+			: 1;
+	describe("\n%i and +");
+
+	PRINTF(("%+d", 0));
+	PRINTF(("%+d", 1));
+	PRINTF(("%+d", -1));
+	PRINTF(("%+d", 24));
+	PRINTF(("%+d", 42));
+	PRINTF(("%+d", -42));
+	PRINTF(("%+d", 2147483647));
+	PRINTF(("%+d", (int)-2147483648));
+	PRINTF(("%+++d", 2147483647));
+	PRINTF(("%++d", (int)-2147483648));
+	PRINTF(("%+d", 2178647));
+	PRINTF(("%+d", (int)-2144348));
+	PRINTF(("%+  d", 2147837));
+	PRINTF(("%+ d", (int)-2147486));
+	PRINTF(("%+ d this is %d getting% di hard :/", (int)-2147486, -2, 42));
 	
 	return (0);
 }
