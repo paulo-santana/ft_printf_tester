84174fbe42af84cdad3d8ff9b933740f0d7be7c4
bnidia <bnidia@bnidia-ubuntu.(none)>
2022-05-07 15:12:42 +0300

    added test for precision from bnidia

--- src/tests.c
+++ src/tests.c
@@ -609,6 +609,7 @@ int run_tests(int test_cat)
 	PRINTF(("%--4.1s %s %------------------9.3s of %s of %-5.7s", "for", "the", "goooood", "aaall", "us"));
 	PRINTF(("%--.sp--.su kkkk", "pegadinha po"));
 	PRINTF(("%-9sScience!", "-42"));
+	PRINTF(("%1.s", "21-school.ru")); //Rustem (bnidia)
 
 	right_cat = (g_all_bonus & CAT_BONUS_1) ? 1
 			: test_cat ? (test_cat & CAT_P && test_cat & CAT_BONUS_1)
